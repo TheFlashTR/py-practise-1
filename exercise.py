fecf15c55a1aae9eb9bb42494c8d5456bf3d6aec
Fl1sh <40336711+TheFlashTR@users.noreply.github.com>
2018-12-09 13:23:11 +0300

    Create exercise.py
    First commit :smile:

---
+++ exercise.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+import feedparser
+import getpass
+import requests
+
+print("**********\nLogin Panel\n**********\n")
+
+sys_user = "root"
+
+sys_pass  = "fl1sh"
+
+username = input("Username: ")
+
+passwd =  getpass.getpass("Pass: ")
+#getpass hides pass.
+
+if (username != sys_user and passwd == sys_pass):
+	print("Wrong Username...")
+elif (username == sys_user and passwd!= sys_pass):
+	print("Wrong Password...")
+elif (username != sys_user and passwd != sys_pass):
+	print("Wrong Username and Password...")
+
+else:
+	print("**********\nMenu\n1-News\n2-Weather\n3-Currency\nq-Exit\n**********\n")
+	operation = input("Select : ")
+	if (operation == '1'):
+	  haber = feedparser.parse('https://www.cnnturk.com/feed/rss/all/news')
+	  for post in haber.entries:
+	  	print(post.title)
+	elif (operation == 'q'):
+		print("Exited...")
+	elif (operation == '2'):
+		city = input('Pls Enter City Name: ')
+		weather = requests.get('https://wttr.in/{}'.format(city+'?lang=tr'), headers={'user agent' : 'curl'})
+		print(weather.text)
+		
+	elif (operation == '3'):
+		print("1 Turkish Liras.")
+		currency = requests.get('https://api.exchangeratesapi.io/latest?base=TRY', headers={'user agent' : 'curl'})
+		print(currency.text)
