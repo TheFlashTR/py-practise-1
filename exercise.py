ac22c8a5b0faf62929a8fc5dba19c8dd24f5c0c4
FL1SH <40336711+TheFlashTR@users.noreply.github.com>
2018-12-31 13:52:28 +0300

    Update exercise.py
    Bugs fixed.

--- exercise.py
+++ exercise.py
@@ -4,6 +4,7 @@ import getpass
 import requests
 
 accounts = []
+islogin = False
 
 def register():
 	print("Register")
@@ -15,20 +16,7 @@ def register():
 		passwd =  getpass.getpass("Pass: ")
 		accounts.append(passwd)
 		return accounts
-		
-def login():
-	username = input("Username: ")
-	passwd =  getpass.getpass("Pass: ")
-	#getpass hides pass.
-	if (username and passwd in accounts):
-	    menu()
-	else:
-		print("Wrong Credentials")
 
-def logout():
-    print("Log outed...")
-    return
-		
 def menu():
 	print("**********\nMenu\n1-News\n2-Weather\n3-Currency\nq-Exit\n**********\n")
 	operation = input("Select : ")
@@ -36,19 +24,37 @@ def menu():
 	  haber = feedparser.parse('https://www.cnnturk.com/feed/rss/all/news')
 	  for post in haber.entries:
 	  	print(post.title)
+	  menu()
 	elif (operation == 'q'):
 		logout()
 	elif (operation == '2'):
 		city = input('Pls Enter City Name: ')
 		weather = requests.get('https://wttr.in/{}'.format(city+'?lang=en'), headers={'user agent' : 'curl'})
 		print(weather.text)
-		
+		menu()
 	elif (operation == '3'):
 		print("1 Turkish Liras.")
 		currency = requests.get('https://api.exchangeratesapi.io/latest?base=TRY', headers={'user agent' : 'curl'})
 		print(currency.text)
+		menu()
 	else:
 	    print("Wrong Choice")
+
+
+def login():
+	username = input("Username: ")
+	passwd =  getpass.getpass("Pass: ")
+	#getpass hides pass.
+	if (username and passwd in accounts):
+		islogin = True
+		menu()
+	else:
+		print("Wrong Credentials")
+
+def logout():
+    print("Log outed...")
+    return
+
 
 while True:
 	print("""**********\nFl1sh's Panel\n1-Login\n2-Register\n**********\n""")
@@ -57,5 +63,3 @@ while True:
 		login()
 	if (stat == '2'):
 		register()
-	else:
-		print("Wrong Credentials")
